053f569616980d19b9417855c15c207e1c7ba1fa
lavanya gollapalli <lavanya.gollapali@sasi.ac.in>
2025-04-18 10:43:22 +0530

    initial

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.getElementById("registerForm").addEventListener("submit",function(event){
     if(!document.getElementById("name").value||
-    !document.getElementById("name").value||
-    !document.getElementById("name").value){
+    !document.getElementById("email").value||
+    !document.getElementById("password").value){
         alert("all files are required!");
         event.preventDefault();
     }
